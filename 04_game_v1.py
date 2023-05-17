64086e426f08e0bd04681ae99612e6a3670fabbe
OnlyNoodles <balakrishn4@middleton.school.nz>
2023-05-17 13:15:22 +1200

    Update 04_game_v1.py

--- 04_game_v1.py
+++ 04_game_v1.py
@@ -41,7 +41,9 @@ easy = [["1", "tahi"], ["2", "rua"], ["3", "toru"], ["4", "wha"], ["5", "rima"],
         ["6", "ono"], ["7", "whetu"], ["8", "waru"], ["9", "iwa"], ["10", "tekau"]]
 medium = [["11", "tekau ma ono"], ["12", "tekau ma rua"],
           ["13", "tekau ma toru"], ["14", "tekau ma wha"],
-          ["15"], ["16"], ["17"], ["18"], ["19"], ["20"]]
+          ["15", "tekau ma rima"], ["16", "tekau ma ono"],
+          ["17", "tekau ma whetu"], ["18", "tekau ma waru"],
+          ["19", "tekau ma iwa"], ["20", "rua tekau"]]
 hard = [["21", "rua tekau ma tahi"], ["22", "rua tekau ma rua"],
         ["23", "rua tekau ma toru"], ["24", "rua tekau ma wha"],
         ["25", "rua tekau ma rima"], ["26", "rua tekau ma ono"],
